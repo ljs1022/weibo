c68dc985a306a4e8ed808ffae35c0d082f15f519
ljs <a020412314@gmail.com>
2020-07-04 14:10:42 +0000

    基礎頁面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '關於')
+@section('content')
+  <h1>關於頁</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '幫助')
+@section('content')
+  <h1>幫助頁</h1>
+@stop
