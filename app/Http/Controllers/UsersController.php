52db5b9834561a0e0543eaab4b00b158fd9f5057
ljs <a020412314@gmail.com>
2020-07-09 13:52:36 +0000

    fix bug

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 class UsersController extends Controller
 {
     public function create()
