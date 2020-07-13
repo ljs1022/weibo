3a40259d42c62a9098d53fc50dffb05b381a41ec
ljs <a020412314@gmail.com>
2020-07-13 13:28:51 +0000

    查看用戶列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store','index']
         ]);
 
         $this->middleware('guest', [
@@ -25,7 +25,7 @@ class UsersController extends Controller
     }
 
     public function show(User $user)
-    {$this->authorize('update', $user);
+    {
         return view('users.show', compact('user'));
     }
 
@@ -73,6 +73,12 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
 }
